7fb974699b852d230f77eacb3c19347b2122a1a4
Ogbonna Vitalis <agavitalisogbonna@gmail.com>
2019-10-30 11:26:44 +0100

    Setting up of migrations and auth files

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\User;
+use App\User\Models;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
