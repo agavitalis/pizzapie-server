7aeef141cc12205df621f7eacf551ffb985b27b1
Ogbonna Vitalis <agavitalisogbonna@gmail.com>
2019-10-30 11:46:21 +0100

    Setting up the configuration parameters

--- app/Http/Controllers/Api/PizzaController.php
+++ app/Http/Controllers/Api/PizzaController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Pizza;
 
 class PizzaController extends Controller
 {
-    //
+   
+    public function getAll()
+    {
+        return Pizza::all();
+    }
+ 
+    public function getOne($id)
+    {
+        return Pizza::find($id);
+    }
+
 }
