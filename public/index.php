cc787017e07d887d967774e6f8a3242958dd24ef
Ogbonna Vitalis <agavitalisogbonna@gmail.com>
2019-11-02 16:05:33 +0100

    Retouched the Readme.md file

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');  
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token, Authorization');   
+//header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token, Authorization');   
 
 
 /**
