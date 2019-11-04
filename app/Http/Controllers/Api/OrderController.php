345d04af4bfec54f2de6ada1d2455b8dff91baee
Ogbonna Vitalis <agavitalisogbonna@gmail.com>
2019-11-04 15:10:48 +0100

    FIxed the bugs discovered by Aya

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -15,7 +15,7 @@ class OrderController extends Controller
 
         foreach ($request->cart as $pizza) {
             Order::create(['user'=>1,'first_name'=>$request->first_name,'last_name'=>$request->last_name,
-            'email'=>$request->email,'address'=>$request->address,'state'=>"ddd",
+            'email'=>$request->email,'address'=>$request->address,'state'=>$request->state,
             'country'=>$request->country,'pizza'=>$pizza['name'],'quantity'=>$pizza['quantity'],
             'price'=>$pizza['price'],'order_code'=>$order_code]);
         }
